f40f04541663098776b12ff8767e7ab7115dd0f3
NANDHA KISHOR <nandhuarrolla3@gmail.com>
2025-06-24 15:42:34 +0530

    Create day5(intro).js

---
+++ day5(intro).js
@@ -0,0 +1,55 @@
+console.log("sai kumar");
+var i=10;
+console.log(i);
+var i=20;
+console.log(i);
+var i=30;
+console.log(i);
+console.log("/* its is let block*/");
+let j=10;
+console.log(j);
+console.log("it is constant block");
+const k=10;
+console.log(k);
+let l=true;
+let f=null;
+let g=undefined;
+let h=Symbol("symbol");
+let s={
+    name:"vattaesh",
+age:"rapper" }
+console.log(l,f,g,h,s);
+console.log(10 & 5);
+console.log(10+20);
+var a=10;
+var b=30;
+console.log("result is",a+b);
+var x=60;
+var y=20;
+
+x +=5;
+y *=2;
+console.log("x +=5;",x);
+console.log(x>y);
+console.log("CONDITIONS")
+if (x>y){
+console.log("a")}
+else{
+  console.log("b")  
+}
+console.log("LOOPS")
+for(x=1;x<10;x++){
+    console.log(x)
+}
+
+
+
+
+
+
+
+
+
+
+
+//ijnkmn000000000000000000000000.00                              
